7cc927f8400025b788a38f98265bdba756c07095
rmane2 <132253739+rmane2@users.noreply.github.com>
2026-04-18 23:47:48 -0700

    Refactor setup.py and update maintainer details
    Updated maintainer information and removed commented code.

--- ras598_assignment_2/setup.py
+++ ras598_assignment_2/setup.py
@@ -1,34 +1,3 @@
-# from setuptools import find_packages, setup
-
-# package_name = 'ras598_assignment_2'
-
-# setup(
-#     name=package_name,
-#     version='0.0.0',
-#     packages=find_packages(exclude=['test']),
-#     data_files=[
-#         ('share/ament_index/resource_index/packages',
-#             ['resource/' + package_name]),
-#         ('share/' + package_name, ['package.xml']),
-#     ],
-#     install_requires=['setuptools'],
-#     zip_safe=True,
-#     maintainer='eva',
-#     maintainer_email='sdhir5@asu.edu',
-#     description='TODO: Package description',
-#     license='TODO: License declaration',
-#     extras_require={
-#         'test': [
-#             'pytest',
-#         ],
-#     },
-#     entry_points={
-#         'console_scripts': [
-#             'planner_node = ras598_assignment_2.planner_node:main'
-#         ],
-#     },
-# )
-
 from setuptools import find_packages, setup
 
 package_name = 'ras598_assignment_2'
@@ -50,8 +19,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='eva',
-    maintainer_email='eva@example.com',
+    maintainer='Rohit Mane',
+    maintainer_email='rmane2@asu.edu',
     description='RAS 598 Assignment 2: Motion Planning',
     license='MIT',
     tests_require=['pytest'],
